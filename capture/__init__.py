c2cca9be1dd54a9e2244e2904f7549a30187d91b
JYAARU <1003129155@qq.com>
2025-12-30 11:44:31 +0900

    Delete capture directory

--- capture/__init__.py
+++
@@ -1,4 +0,0 @@
-from .capture_service import CaptureService
-from .window_finder import WindowFinder, is_smart_selection_available
-
-__all__ = ['CaptureService', 'WindowFinder', 'is_smart_selection_available']
